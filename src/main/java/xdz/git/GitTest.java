7ae0b1f6d103aceaf57ec55a24ab7acdcfe3508f
xdz <2714091567@qq.com>
2022-12-30 22:10:03 +0800

    push commit

--- src/main/java/xdz/git/GitTest.java
+++ src/main/java/xdz/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello,git4");
         System.out.println("master com");
         System.out.println("got-fix comm");
+        System.out.println("push comm");
     }
 }
